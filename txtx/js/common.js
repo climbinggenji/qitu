4699d21a3d17017d57ecd86cbb9b5872c8fa8177
pipil <786924514@qq.com>
2020-07-10 17:52:37 +0800

    公共组件提取，刺激页面添加2020-07-10

---
+++ txtx/js/common.js
@@ -0,0 +1,9 @@
+// 监听滚动固定顶部导航栏
+window.addEventListener('scroll', function() {
+    var top = $('body, html').scrollTop();
+    if (top>340) {
+        $('#header').addClass('header-fixed');
+    } else {
+        $('#header').removeClass('header-fixed');
+    }
+})
