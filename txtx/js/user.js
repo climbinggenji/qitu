94683216fa5389cb1a978c482fa7f4febf394da7
pipil <786924514@qq.com>
2020-07-23 18:13:42 +0800

    个人中心拆分，专题页及专题详情

--- txtx/js/user.js
+++ txtx/js/user.js
@@ -1,24 +1,17 @@
 // 菜单栏切换
 $('.user-menu>ul>li').on('click', function() {
     if (!$(this).hasClass('active')) {
-        $('.provide-menu-list>li').removeClass('active');
-        if ($(this).index() < 3) {
-            $(this).addClass('active').siblings().removeClass('active');
-            $('.main-content>div:eq('+ $(this).index() + ')').css('display', 'block').siblings().css('display', 'none');
-        } else if ($(this).index() > 3) {
-            $(this).addClass('active').siblings().removeClass('active');
-            $('.main-content>div:eq('+ ($(this).index()-1) + ')').css('display', 'block').siblings().css('display', 'none');
-        } else {
+        if ($(this).index() == 3) {
             $('.provide-menu-list').toggle();
         }
     }
 })
 
-$('.provide-menu-list>li').on('click', function() {
-    if (!$(this).hasClass('active')) {
-        $('.user-menu>ul>li').removeClass('active');
-        $(this).addClass('active').siblings().removeClass('active');
-        $('.provide-content').show().siblings().hide();
-        $('.provide-content>div:eq('+ $(this).index() + ')').show().siblings().hide();
-    }
-})
+// $('.provide-menu-list>li').on('click', function() {
+//     if (!$(this).hasClass('active')) {
+//         $('.user-menu>ul>li').removeClass('active');
+//         $(this).addClass('active').siblings().removeClass('active');
+//         $('.provide-content').show().siblings().hide();
+//         $('.provide-content>div:eq('+ $(this).index() + ')').show().siblings().hide();
+//     }
+// })
--- txtx/js/vip-purchase.js
+++ txtx/js/vip-purchase.js
@@ -2,16 +2,6 @@ var purchaseVal = {
     type: 0,
     price: 99
 }
-// vip类型
-$('.purchase-tab-option').on('click', function() {
-    if (!$(this).hasClass('active')) {
-        purchaseVal.type = $(this).index();
-        console.log(purchaseVal)
-        $(this).addClass('active').siblings().removeClass('active');
-        $('.purchase-type-box:eq('+ $(this).index() +')').show().siblings().hide();
-    }
-})
-// 价格
 $('.purchase-item').on('click', function() {
     if (!$(this).hasClass('active')) {
         $(this).addClass('active').siblings().removeClass('active');
